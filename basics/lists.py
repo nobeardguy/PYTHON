99337959390b46790886021a89b95c8c7187aac8
Eric <ericgits001@gmail.com>
2026-06-03 22:46:57 +0300

    python lists, tuples

---
+++ basics/lists.py
@@ -0,0 +1,56 @@
+# Lists store multiple items in a single variable, and they are ordered and changeable.
+thislist = ["apple", "banana", "cherry"] 
+print(thislist) 
+
+#creating a list with the list() constructor
+fruits = list(("banana","Watermelon","grapes")) 
+print(fruits) 
+
+#looping through a list
+people = ["Hassan","KG","Musa"] 
+for x in people:
+    print(x)
+#list comprehension
+people = ["Hassan","KG","Musa"]
+newlist = [x for x in people if "a" not in x]
+
+for x in newlist:
+    print(f"{x} is the new number {1}") 
+
+import numpy as np 
+# Exercises
+counties = list(("Meru","Garissa","Nairobi","machakos","kisumu"))
+favcounties = counties[0],counties[4] 
+
+print(favcounties)  #print the 1st and last item
+
+#add one more county and remove one
+counties.append("Mombasa")
+counties.remove("machakos")
+print(counties)
+
+scores = [45,78,56,89,90]
+ave = np.mean(scores)      # average of the scores 
+print(ave) 
+
+descScores = sorted(scores,reverse=True) # sorting the scores in descending order
+print(descScores) 
+
+# replace score <50 with Fail
+for i in range(len(scores)):
+    if scores[i] < 50:
+        scores[i] = "Fail"
+        print(scores)
+
+userinput = (23,34,100,54,45)
+
+# a fn that takes a tuple of scores, converts it to a list, and then finds the max and min scores.
+def convert_to_list(userinput):
+    userlist = list(userinput) 
+    max_score = np.max(userlist)
+    min_score = np.min(userlist)
+    print(f"The maximum score is: {max_score} and the minimum score is: {min_score}")
+
+
+
+convert_to_list(userinput) 
---
+++ basics/tuples.py
@@ -0,0 +1,41 @@
+#Tuples
+# tuples are ordered and unchangeable, & they allow duplicate values.they use parantheses,()
+thistuple = ("volvo","bmw","ford")
+print(thistuple)
+
+#creating a tuple with the tuple() constructor
+thistuple = tuple(("volvo","mazda","honda"))
+print (thistuple) 
+
+print(type(thistuple))
+# Adding items to a tuple is not possible, but you can convert the tuple into a list, add the item, and convert it back into a tuple.
+y= list(thistuple)
+y.append("toyota") 
+y.remove("mazda")    # removing an item from the list
+
+#converting back to tuple
+thistuple = tuple(y)
+print(f"The updated tuple is: {thistuple}") 
+
+print(type(thistuple))
+
+# Excercises: create a tuple & access the 3rd item
+months = tuple(("Jan","Feb","Mar","Apr", "May"))
+third_month = months[2]
+print(third_month)
+
+# unpacking a tuple 
+student = ("Eric", 24, "Mombasa")
+name = student[0]
+age = student[1]
+location = student[2]
+print(f"The student's name is {name}, he is {age} years old and live in {location}")
+
+# concanetating tuples
+fruits = ("apple","melon","grapes","banana")
+vegetables = ("cabbage","spinach","kale")
+
+combined = fruits + vegetables 
+total_no_of_items =  len(combined)      #finding the total no. of itemms in the combined tuples
+print(f"The combined tuple has {total_no_of_items} items: {combined}") 
+
