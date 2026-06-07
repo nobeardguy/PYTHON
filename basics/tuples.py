b4379c1f7d5a3f104535414193693b90dd9da8d0
Eric <ericgits001@gmail.com>
2026-06-07 22:37:10 +0300

    Tuples practice

--- basics/tuples.py
+++ basics/tuples.py
@@ -29,7 +29,7 @@ student = ("Eric", 24, "Mombasa")
 name = student[0]
 age = student[1]
 location = student[2]
-print(f"The student's name is {name}, he is {age} years old and live in {location}")
+print(f"The student's name is {name}, he is {age} years old and lives in {location}")
 
 # concanetating tuples
 fruits = ("apple","melon","grapes","banana")
@@ -38,3 +38,27 @@ vegetables = ("cabbage","spinach","kale")
 combined = fruits + vegetables 
 total_no_of_items =  len(combined)      #finding the total no. of itemms in the combined tuples
 print(f"The combined tuple has {total_no_of_items} items: {combined}") 
+
+#Create a tuple of 7 days of the week. Print the weekend days only.
+weekdays = tuple(("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")) 
+
+weekend = weekdays[5],weekdays[6]
+print(f"Weekend days are: {weekend}") 
+
+#A program that takes a tuple of exam scores and finds the average without converting it to a list.
+exam_scores = (54,56,78,90,75)
+def average(scores):
+    total = sum(scores)
+    count = len(scores)
+    avg = total/count
+    print(f"The average score is: {avg}")
+
+average(exam_scores)
+
+#Combine two tuples of numbers and find the index of a specific number in the combined tuple.
+tuple1 = (12,13,14,15)
+tuple2 = (16,17,18,19)
+
+combined = tuple1+ tuple2
+index_of_number = combined.index(17)
+print(f"The index of number 17 in the combined tuple is: {index_of_number}") 
